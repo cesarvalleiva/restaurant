36524423ce5918617d29fa0069288819349fca57
Cesar Val Leiva <cesarvalleiva@gmail.com>
2022-01-16 20:26:23 -0300

    Render dinámica proveniente de la API en Buscadas

--- src/components/Opciones/Buscadas.js
+++ src/components/Opciones/Buscadas.js
@@ -1,57 +1,34 @@
+import { useContext, useEffect } from 'react'
 import Hamburguesa2 from '../../assets/img/hamburguesa2.png'
 import Hamburguesa3 from '../../assets/img/hamburguesa3.png'
 import Hamburguesa4 from '../../assets/img/hamburguesa4.png'
+import { Context } from '../../store/burgerContext'
 import './Buscadas.css'
 
 const Buscadas = () => {
+    const {info, obtenerInfo} = useContext(Context)
+
+    useEffect(() => {
+        obtenerInfo('buscadas')
+    }, [])
+
     return ( 
         <div className='containerOpciones'>
             <h2>Las más buscadas</h2>
             <div className='container contenedorOpciones'>
-                <div className="card shadow buscadas">
-                    <img src="https://i.pinimg.com/originals/76/e6/e8/76e6e8253a7b5225f4d0ddfe37b0faaf.jpg" className="card-img-top imgOpciones" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <div className='d-flex justify-content-between'>
-                            <p className="precio">$790</p>
-                            <a href="#" className="btn btn-warning">Ordenar</a>
+                {info.map(buscada => (
+                    <div key={buscada.id} className="card shadow buscadas">
+                        <img src="https://i.pinimg.com/originals/76/e6/e8/76e6e8253a7b5225f4d0ddfe37b0faaf.jpg" className="card-img-top imgOpciones" alt={buscada.nombre} />
+                        <div className="card-body">
+                            <h5 className="card-title h5Buscadas ">{buscada.nombre}</h5>
+                            <div className="card-text">{buscada.ingredientes.map((ingre, idx) => (<p className='m-0' key={idx}>{`- ${ingre}`}</p>))}</div>
+                            <div className='d-flex justify-content-between mt-4'>
+                                <p className="precio">${buscada.precio}</p>
+                                <a href="#" className="btn btn-warning">Ordenar</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="card shadow buscadas">
-                    <img src={Hamburguesa2} className="card-img-top imgOpciones" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <div className='d-flex justify-content-between'>
-                            <p className="precio">$590</p>
-                            <a href="#" className="btn btn-warning">Ordenar</a>
-                        </div>
-                    </div>
-                </div>
-                <div className="card shadow buscadas">
-                    <img src={Hamburguesa3} className="card-img-top imgOpciones" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <div className='d-flex justify-content-between'>
-                            <p className="precio">$650</p>
-                            <a href="#" className="btn btn-warning">Ordenar</a>
-                        </div>
-                    </div>
-                </div>
-                <div className="card shadow buscadas">
-                    <img src={Hamburguesa4} className="card-img-top imgOpciones" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <div className='d-flex justify-content-between'>
-                            <p className="precio">$890</p>
-                            <a href="#" className="btn btn-warning">Ordenar</a>
-                        </div>
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
      );
--- src/store/burgerContext.js
+++ src/store/burgerContext.js
@@ -1,11 +1,18 @@
 import { createContext, useState } from "react";
+import axios from 'axios'
 
 export const Context = createContext(null);
 
 const BurgerContext = ({children}) => {
-    const [burger, setBurger] = useState('chau')
+    const [info, setinfo] = useState([])
+
+    const obtenerInfo = async (endpoint) => {
+        const response = await axios.get(`https://apipdtc.herokuapp.com/bulldog/${endpoint}`)
+        setinfo(response.data)
+    }
+
     return ( 
-        <Context.Provider value={{burger}}>
+        <Context.Provider value={{info, obtenerInfo}}>
             {children}
         </Context.Provider>
      );
