5f32992133c5a5b8de98f0437ac1d1e90722fa53
Cesar Val Leiva <cesarvalleiva@gmail.com>
2022-01-16 20:39:30 -0300

    Render dinámica proveniente de la API en Premium

--- src/components/Premium/Premium.js
+++ src/components/Premium/Premium.js
@@ -1,80 +1,37 @@
+import { useContext, useEffect } from 'react';
+import { Context } from '../../store/burgerContext';
 import './Premium.css'
 
 const Premium = () => {
+    const {info, obtenerInfo} = useContext(Context);
+
+    useEffect(() => {
+        obtenerInfo('premium')
+    }, [])
+    
     return ( 
         <div className='contenedorTotalPremium'>
             <h2 className='tituloPremium'>Las Premium</h2>
             <div className='container contenedorPremium'>
-                <div className="card hamburguesaPremium shadow border-0">
-                    <div className="row g-0">
-                        <div className="col-md-5 contenedorImagenPremium">
-                            <img src="https://okdiario.com/img/2021/03/06/4-hamburguesas-gourmet.jpg" className="img-fluid rounded-start" alt="..." />
-                        </div>
-                        <div className="col-md-7">
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                                <div className='w-100 d-flex justify-content-between'>
-                                    <p className="precio">$590</p>
-                                    <button className='btn btn-warning'>Ordenar</button>
+                {info.map(premium => (
+                    <div className="card hamburguesaPremium shadow">
+                        <div className="row g-0" key={premium.id}>
+                            <div className="col-md-5 contenedorImagenPremium">
+                                <img src="https://okdiario.com/img/2021/03/06/4-hamburguesas-gourmet.jpg" className="img-fluid rounded-start" alt={premium.nombre} />
+                            </div>
+                            <div className="col-md-7">
+                                <div className="card-body">
+                                    <h5 className="card-title">{premium.nombre}</h5>
+                                    <div className="card-text d-flex ingredientesPremium">{premium.ingredientes.map((ingre, idx) => (<p className='m-0 mb-3' key={idx}>{`${ingre}. `}</p>))}</div>
+                                    <div className='w-100 d-flex justify-content-between'>
+                                        <p className="precio">${premium.precio}</p>
+                                        <button className='btn btn-warning'>Ordenar</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="card hamburguesaPremium shadow border-0">
-                    <div className="row g-0">
-                        <div className="col-md-5 contenedorImagenPremium">
-                            <img src="https://dosflorines.com.ar/wp-content/uploads/2020/11/hamburgueasa-gourmet.jpg" className="img-fluid rounded-start" alt="..." />
-                        </div>
-                        <div className="col-md-7">
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                                <div className='w-100 d-flex justify-content-between'>
-                                    <p className="precio">$590</p>
-                                    <button className='btn btn-warning'>Ordenar</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className='container contenedorPremium'>
-                <div className="card hamburguesaPremium shadow border-0">
-                    <div className="row g-0">
-                        <div className="col-md-5 contenedorImagenPremium">
-                            <img src="https://dosflorines.com.ar/wp-content/uploads/2020/11/hamburgueasa-gourmet.jpg" className="img-fluid rounded-start" alt="..." />
-                        </div>
-                        <div className="col-md-7">
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                                <div className='w-100 d-flex justify-content-between'>
-                                    <p className="precio">$590</p>
-                                    <button className='btn btn-warning'>Ordenar</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="card hamburguesaPremium shadow border-0">
-                    <div className="row g-0">
-                        <div className="col-md-5 contenedorImagenPremium">
-                            <img src="https://okdiario.com/img/2021/03/06/4-hamburguesas-gourmet.jpg" className="img-fluid rounded-start" alt="..." />
-                        </div>
-                        <div className="col-md-7">
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                                <div className='w-100 d-flex justify-content-between'>
-                                    <p className="precio">$590</p>
-                                    <button className='btn btn-warning'>Ordenar</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
      );
